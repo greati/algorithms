bf70503208686a21f34f4987d11cf6b4e9b3ed30
Vitor Greati <greati@ufrn.edu.br>
2015-12-25 14:23:14 -0300

    Maximum-subarray problem by divide and conquer

---
+++ Implementations/DivideAndConquer/max-subarray.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <limits.h>
+
+using namespace std;
+
+struct subarray {
+	int begin, end, sum;
+	subarray (int b, int e, int s) : begin {b}, end {e}, sum {s} {};
+};
+
+subarray find_max_crossing_subarray (int * A, int low, int mid, int high) {
+	int temp_sum = 0;
+	int max_left = INT_MIN;
+	int i_max_left = -1;
+	for (int i = mid; i >= low; --i) {
+		temp_sum += A[i];
+		if (temp_sum >= max_left) {
+			max_left = temp_sum;
+			i_max_left = i;
+		}	
+	}
+	
+	temp_sum = 0;
+	int max_right = INT_MIN;
+	int j_max_right = -1;
+	for (int j = mid + 1; j <= high; ++j) {
+		temp_sum += A[j];
+		if (temp_sum >= max_right) {
+			max_right = temp_sum;
+			j_max_right = j;
+		}	
+	}
+
+	return {i_max_left, j_max_right, max_left + max_right};
+}
+
+subarray find_maximum_subarray(int * A, int low, int high) {
+	if (high == low)
+		return {low, high, A[low]};
+	else {
+		int mid = low + (high - low) / 2;
+		subarray maxleft = find_maximum_subarray(A, low, mid);
+		subarray maxright = find_maximum_subarray(A, mid + 1, high);
+		subarray maxcross = find_max_crossing_subarray(A, low, mid, high);
+		if (maxleft.sum > maxright.sum && maxleft.sum > maxcross.sum)
+			return maxleft;
+		if (maxright.sum > maxleft.sum && maxright.sum > maxcross.sum)
+			return maxright;
+		else return maxcross;
+	}
+} 
+
+int main(void) {
+
+	int test[] = {13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7};
+
+	subarray res = find_maximum_subarray(test, 0, sizeof(test)/sizeof(int) - 1);
+	
+	cout << "Maximum-subarray: [ "<< res.begin << " ; " << res.end << " ] // sum = " <<res.sum << std::endl;
+
+	return 0;
+}
