6dc47119b055b3e0adbc0f50a82ce78d8bf34214
Vitor Greati <greati@ufrn.edu.br>
2016-01-21 18:01:28 -0300

    RBT rotations

--- Implementations/data_structures/red_black_tree/src/rbt.cpp
+++ Implementations/data_structures/red_black_tree/src/rbt.cpp
@@ -1,1 +1,48 @@
 #include "rbt.h"
+
+template<typename TKey, typename TData, typename TLessComp>
+void RedBlackTree<TKey, TData, TLessComp>::left_rotate(TreeNode * node) {
+	if (node->right == nil) return;
+
+	TreeNode * newRoot = node->right;
+
+	node->right = newRoot->left;	
+	if (newRoot->left != nil)
+		newRoot->left->parent = node;
+
+	newRoot->parent = node->parent;
+
+	if (node->parent == nil)
+		root = newRoot;
+	else if (node->parent->left == node)
+		node->parent->left = newRoot;
+	else
+		node->parent->right = newRoot;
+
+	newRoot->left = node;
+	node->parent = newRoot;
+}
+
+
+template<typename TKey, typename TData, typename TLessComp>
+void RedBlackTree<TKey, TData, TLessComp>::right_rotate(TreeNode * node) {
+	if (node->left == nil) return;
+	
+	TreeNode * newRoot = node->left;
+
+	node->left = newRoot->right;
+	if (newRoot->right != nil)
+		newRoot->right->parent = node;
+
+	newRoot->parent = node->parent;
+
+	if (node->parent == nil)
+		root = newRoot;
+	else if (node->parent->left == node)
+		node->parent->left = newRoot;
+	else
+		node->parent->right = newRoot;
+
+	newRoot->left = node;
+	node->parent = newRoot;
+}
