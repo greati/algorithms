3c17582d0564083b15e4015bdc6589fbba5093bb
Vitor Greati <greati@ufrn.edu.br>
2016-01-22 23:11:59 -0300

    Fix up insert

--- Implementations/data_structures/red_black_tree/src/rbt.cpp
+++ Implementations/data_structures/red_black_tree/src/rbt.cpp
@@ -74,3 +74,47 @@ TData * RedBlackTree<TKey, TData, TLessComp>::insert(const TKey & newKey, TData 
 
 	return &(newNode->data);
 }	
+
+template<typename TKey, typename TData, typename TLessComp>
+void RedBlackTree<TKey, TData, TLessComp>::fix_up(TreeNode * node) {
+	TreeNode * other = nullptr;
+	// Look: I'm RED! So, while I have a RED parent, something must be done,
+	// as I'm violating a RBT property..
+	// Also, if I'm the root, another property is broken, since
+	// root must be BLACK!
+	while (node->parent->color == Color::RED) {
+		// If the parent is the left child of its parent
+		if (node->parent == node->parent->parent->left) {
+			other = node->parent->parent->right;	
+			if (other->color == Color::RED) {
+				node->parent->color = Color::BLACK;
+				other->color = Color::BLACK;
+				node->parent->parent->color = Color::RED;
+				node = node->parent->parent;
+			} else if (node == node->parent->right) {
+				node = node->parent;
+				left_rotate(node);
+			} else {
+				node->parent->color = Color::BLACK;
+				node->parent->parent->color = Color::RED;
+				right_rotate(node->parent->parent);
+			}
+		} else {
+			other = node->parent->parent->left;	
+			if (other->color == Color::RED) {
+				node->parent->color = Color::BLACK;
+				other->color = Color::BLACK;
+				node->parent->parent->color = Color::RED;
+				node = node->parent->parent;
+			} else if (node == node->parent->left) {
+				node = node->parent;
+				right_rotate(node);
+			} else {
+				node->parent->color = Color::BLACK;
+				node->parent->parent->color = Color::RED;
+				left_rotate(node->parent->parent);
+			}
+		}	
+	}
+	root->color = Color::BLACK;
+}
