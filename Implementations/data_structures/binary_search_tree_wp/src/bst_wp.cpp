7d0e51872165d45d250465003db70dd636ef0960
Vitor Greati <greati@ufrn.edu.br>
2016-04-12 22:49:44 -0300

    Search for node done

--- Implementations/data_structures/binary_search_tree_wp/src/bst_wp.cpp
+++ Implementations/data_structures/binary_search_tree_wp/src/bst_wp.cpp
@@ -1,8 +1,11 @@
 #include "bst_wp.h"
 
 template<typename TKey, typename TData, typename TComp>
-typename BinarySearchTreeWP<TKey,TData,TComp>::Node * BinarySearchTreeWP<TKey,TData,TComp>::searchNode(const TKey & key) const {
-
+typename BinarySearchTreeWP<TKey,TData,TComp>::Node * BinarySearchTreeWP<TKey,TData,TComp>::searchNode(const TKey & key, Node * parentReceiver) const {
+	Node * i { root };
+	while (i != nullptr and not (not comp(key, i->key) and not comp(i->key, key))) 
+		i = comp(key, i->key) ?  i->left : i->right;
+	return i;
 }
 
 template<typename TKey, typename TData, typename TComp>
